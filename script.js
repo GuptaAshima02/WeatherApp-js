588c7aed1e43959bb3bf398910c4d34c38febb33
Ashima <guptaashima30@gmail.com>
2023-05-16 01:09:40 +0530

    Weather app using fetch api and promises

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 // `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-let defaultCity="jammu";
 const input = document.getElementById("input");
 const submitBtn = document.getElementById("submit");
 
 const cityName = document.getElementById("cityname");
 const temp = document.getElementById("temp");
 const humidity = document.getElementById("humidity");
+const error = document.getElementById("error");
 
 function getWeather(namee) {
   fetch( `https://api.openweathermap.org/data/2.5/weather?q=${namee}&appid=dda26109be2e21c827819fa96320454e&units=metric`)
     .then(response => {
-      if (!response.ok) {
-        throw new Error("City not found.");
-      }
       return response.json();
     })
     .then(data => {
-      console.log(data.name);
       cityName.textContent = data.name;
       temp.textContent = `Temperature: ${data.main.temp}°C`;
       humidity.textContent = `Humidity: ${data.main.humidity}%`;
       wind.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+      error.textContent = "";
     })
-    .catch(error =>{
-        console.error(error);
-    })
+    .catch(errorr => {
+      cityName.textContent = "";
+      temp.textContent = "";
+      humidity.textContent = "";
+      wind.textContent = "";
+      error.textContent = "City not found.";
+    });
 }
-
-getWeather(defaultCity);
-
+getWeather("jammu");
 submitBtn.addEventListener('click', () => {
     const city = input.value.trim();
     if (city) {
